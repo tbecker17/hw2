74cfcdacc667ce2c85d1aab5f1e41154e5d004d3
Tim Becker <tim.becker@kellogg.northwestern.edu>
2023-05-01 23:01:24 +0000

    Created tables

--- db/migrate/20230501225359_create_movies.rb
+++ db/migrate/20230501225359_create_movies.rb
@@ -1,7 +1,10 @@
 class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
-
+      t.string "title"
+      t.integer "year_released"
+      t.string "rated"
+      t.references "studio", null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20230501225415_create_roles.rb
+++ db/migrate/20230501225415_create_roles.rb
@@ -1,7 +1,9 @@
 class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
-
+      t.references :movie, null: false, foreign_key: true
+      t.references :actor, null: false, foreign_key: true
+      t.string "character_name"
       t.timestamps
     end
   end
